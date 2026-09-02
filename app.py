a8e68a67da45389f04c2eae2be6f302a1043c129
bapolo24 <byronjoseapoloaguilar@gmail.com>
2026-09-02 18:33:01 -0500

    Fix typo in variable name for reservoir page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st  # Importa Streamlit para controlar la navegación genera
 inicio = st.Page("inicio.py", title="Home", icon="🏠", default=True)  # Define la página principal y la establece como predeterminada.
 Produccion = st.Page("produccion.py", title="Produccion", icon="🛢️")  # Define la página destinada al cálculo del grado API.
 Perforacion = st.Page("perforacion.py", title="Perforacion", icon="📊")  # Define una página adicional para visualizar datos precargados.
-Rerservorios = st.Page("reservorios.py", title="Reservorios", icon="📊")  # Define una página adicional para visualizar datos precargados.
+Reservorios = st.Page("reservorios.py", title="Reservorios", icon="📊")  # Define una página adicional para visualizar datos precargados.
 paginas = {"Principal": [inicio], "Ejercicios": [Produccion, Perforacion, Reservorios]}  # Agrupa las páginas en secciones dentro de la navegación.
 pagina = st.navigation(paginas)  # Construye el menú multipágina utilizando los grupos definidos.
 pagina.run()  # Ejecuta solamente la página elegida por el usuario.
